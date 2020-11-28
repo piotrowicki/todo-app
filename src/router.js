9233cb7333949c8c6d635ebe49a30fc4f36cf693
Piotr Nowicki <piotrowicki@gmail.com>
2020-11-28 18:10:18 +0100

    Fix router paths

--- src/router.js
+++ src/router.js
@@ -1,36 +1,24 @@
 import Vue from "vue";
 import Router from "vue-router";
 
-import TasksList from './components/task/TasksList'
-import TaskDetails from './components/task/TaskDetails'
-import TaskEdit from './components/task/TaskEdit'
-import TasksNew from './components/task/TaskNew'
+import List from './components/task/TasksList'
+import Details from './components/task/TaskDetails'
+import Edit from './components/task/TaskEdit'
+import New from './components/task/TaskNew'
 
 Vue.use(Router);
 
 export default new Router({
     mode: "history",
     routes: [
+        { path: '/', redirect: '/tasks' },
+        { path: '/tasks', component: List },
+        { path: '/tasks/new', component: New },
         {
-            path: "/",
-            alias: "/tasks",
-            name: "tasks",
-            component: TasksList
-        },
-        {
-            path: "/tasks/:id",
-            name: "task-details",
-            component: TaskDetails
-        },
-        {
-            path: "/tasks/:id/edit",
-            name: "task-edit",
-            component: TaskEdit
-        },
-        {
-            path: "/tasks/new",
-            name: "task-new",
-            component: TasksNew
+            path: '/tasks/:id', component: Details,
+            children: [
+                { path: '/tasks/:id/edit', component: Edit }
+            ]
         }
     ]
 });
