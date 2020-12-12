79bed306b0f39c73f640459e3bb169c34ec5fb52
Piotr Nowicki <piotrowicki@gmail.com>
2020-12-12 16:26:22 +0100

    Fix Task New layout

--- src/components/task/TaskNew.vue
+++ src/components/task/TaskNew.vue
@@ -1,39 +1,45 @@
 <template>
   <b-container fluid="sm">
-    <b-form @submit="onSave">
-      <b-form-group
-        id="title-group"
-        label="Title:"
-        label-for="title"
-        description="The title of your task."
-      >
-        <b-form-input
-          id="title"
-          v-model="task.title"
-          type="text"
-          required
-        ></b-form-input>
-      </b-form-group>
+    <b-row>
+      <b-col></b-col>
+      <b-col>
+        <b-form @submit="onSave">
+          <b-form-group
+            id="title-group"
+            label="Title:"
+            label-for="title"
+            description="The title of your task."
+          >
+            <b-form-input
+              id="title"
+              v-model="task.title"
+              type="text"
+              required
+            ></b-form-input>
+          </b-form-group>
 
-      <b-form-group
-        id="description-group"
-        label="Description:"
-        label-for="description"
-      >
-        <b-form-textarea
-          id="description"
-          v-model="task.description"
-          required
-          rows="3"
-          max-rows="6"
-        ></b-form-textarea>
-      </b-form-group>
-      <b-button-group>
-        <b-button type="submit" variant="primary">Save</b-button>
-        <b-button type="reset" to="/tasks" variant="danger">Back</b-button>
-      </b-button-group>
-    </b-form>
-     <b-alert variant="success" show v-if="msg">{{ msg }}</b-alert>
+          <b-form-group
+            id="description-group"
+            label="Description:"
+            label-for="description"
+          >
+            <b-form-textarea
+              id="description"
+              v-model="task.description"
+              required
+              rows="3"
+              max-rows="6"
+            ></b-form-textarea>
+          </b-form-group>
+          <b-button-group>
+            <b-button type="submit" variant="primary">Save</b-button>
+            <b-button type="reset" to="/tasks" variant="danger">Back</b-button>
+          </b-button-group>
+        </b-form>
+        <b-alert variant="success" show v-if="msg">{{ msg }}</b-alert>
+      </b-col>
+      <b-col></b-col>
+    </b-row>
   </b-container>
 </template>
 
@@ -44,7 +50,7 @@ export default {
   data() {
     return {
       task: {},
-      msg: ''
+      msg: "",
     };
   },
   methods: {
@@ -53,12 +59,12 @@ export default {
       TaskDataService.save(this.task)
         .then((response) => {
           console.log(response);
-          this.msg = "Task added successfully."
+          this.msg = "Task added successfully.";
         })
         .catch((e) => {
           console.log(e);
         });
     },
-  }
+  },
 };
 </script>
